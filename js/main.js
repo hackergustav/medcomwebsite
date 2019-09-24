7301bc94a947b4cb2eaff6c3fc4479027157f57f
alean378 <alean378@student.liu.se>
2019-09-24 11:42:12 +0200

    Format employees
    Added all profile images placeholder so it will be easy to reaplce later whne eveyrone has submited. Also created an script to generate this easily for maintance

--- js/main.js
+++ js/main.js
@@ -230,18 +230,6 @@ $(document).ready(function () {
     time: 1000
   });
 
-  // Porfolio isotope and filter
-  $(window).on('load', function () {
-    var portfolioIsotope = $('.portfolio-container').isotope({
-      itemSelector: '.portfolio-item'
-    });
-    $('#portfolio-flters li').on( 'click', function() {
-      $("#portfolio-flters li").removeClass('filter-active');
-      $(this).addClass('filter-active');
-  
-      portfolioIsotope.isotope({ filter: $(this).data('filter') });
-    });
-  });
 
 
 
